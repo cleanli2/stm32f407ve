071bb015616a91e63a436d6a455a0e29a17b2d77
CleanLi <clean_li@hotmail.com>
2025-12-13 14:18:10 +0800

    Revert "lcd clear 48ms"
    This reverts commit d40befc1624cf0ad4ffe8c9b4428d603523b2180.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -410,12 +410,12 @@ static void MX_FSMC_Init(void)
   hsram1.Init.WriteBurst = FSMC_WRITE_BURST_DISABLE;
   hsram1.Init.PageSize = FSMC_PAGE_SIZE_NONE;
   /* Timing */
-  Timing.AddressSetupTime = 4;
-  Timing.AddressHoldTime = 4;
-  Timing.DataSetupTime = 9;
-  Timing.BusTurnAroundDuration = 4;
-  Timing.CLKDivision = 4;
-  Timing.DataLatency = 4;
+  Timing.AddressSetupTime = 15;
+  Timing.AddressHoldTime = 15;
+  Timing.DataSetupTime = 255;
+  Timing.BusTurnAroundDuration = 15;
+  Timing.CLKDivision = 16;
+  Timing.DataLatency = 17;
   Timing.AccessMode = FSMC_ACCESS_MODE_A;
   /* ExtTiming */
 
