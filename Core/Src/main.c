56549746d056c9523724af88512717ada4c89f72
CleanLi <clean_li@hotmail.com>
2025-12-16 21:58:35 +0800

    optimize FSMC para

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -410,10 +410,10 @@ static void MX_FSMC_Init(void)
   hsram1.Init.WriteBurst = FSMC_WRITE_BURST_DISABLE;
   hsram1.Init.PageSize = FSMC_PAGE_SIZE_NONE;
   /* Timing */
-  Timing.AddressSetupTime = 3;
+  Timing.AddressSetupTime = 2;
   Timing.AddressHoldTime = 15;
-  Timing.DataSetupTime = 6;
-  Timing.BusTurnAroundDuration = 3;
+  Timing.DataSetupTime = 4;
+  Timing.BusTurnAroundDuration = 2;
   Timing.CLKDivision = 16;
   Timing.DataLatency = 17;
   Timing.AccessMode = FSMC_ACCESS_MODE_A;
